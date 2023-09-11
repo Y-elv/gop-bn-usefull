c90465cf1e2de9f0b2eb6bf6cdae9dc6d699c419
Y-elv <mugishaelvis456@gmail.com>
2023-09-11 15:14:43 +0200

    progress

--- CRUD/handlers/article_handler.go
+++ CRUD/handlers/article_handler.go
@@ -1,32 +1,34 @@
 package handlers
+
 import (
 	"fmt"
-	"net/http")
+	"net/http"
+)
 
 func GetAllArticles(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Get all articles")
 }
 
 func GetArticleByID(w http.ResponseWriter, r *http.Request) {
-	
+
 	fmt.Fprintln(w, "Get article by ID")
 }
 
 func CreateArticle(w http.ResponseWriter, r *http.Request) {
-	
+
 	fmt.Fprintln(w, "Create article")
 }
 
 func UpdateArticle(w http.ResponseWriter, r *http.Request) {
-	
+
 	fmt.Fprintln(w, "Update article")
 }
 
 func DeleteArticle(w http.ResponseWriter, r *http.Request) {
-	
+
 	fmt.Fprintln(w, "Delete article")
 }
 func DeleteAllArticle(w http.ResponseWriter, r *http.Request) {
-	
+
 	fmt.Fprintln(w, "Delete all article")
 }
